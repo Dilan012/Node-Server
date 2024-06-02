b507c2d0417c46c112129f403748f2f7bff2cfba
Dilan Madhusankha <dilanmadusanka405@gmail.com>
2024-06-02 23:11:37 +0530

    add another endpoint for newly created endpoint

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ app.get('/', (req, res) => {
 
 app.get('/test2',(req,res) =>{
 <<<<<<< HEAD
+<<<<<<< HEAD
     res.send("another endpoint")
 =======
     res.send("test2 endpoint")
 >>>>>>> 6ce419e (add another endpoint with get methos)
+=======
+    res.send("another endpoint")
+>>>>>>> 1e5a64e (add another endpoint for newly created endpoint)
 })
 
 app.listen(port, () => {
